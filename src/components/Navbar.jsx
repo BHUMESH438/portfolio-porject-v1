c07a3fc997258e0daea30618ee495b2deb0c2789
Bhumesh <bhumesh1719@gmail.com>
2023-10-10 09:36:50 +0530

    nav link reset

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,3 @@
-import { links } from '../data';
-
 const Navbar = () => {
   return (
     <nav className='bg-emerald-100 sticky top-0'>
@@ -8,16 +6,6 @@ const Navbar = () => {
           Web
           <span className='text-emerald-600'>Dev</span>
         </h2>
-        <div className='flex gap-x-3 '>
-          {links.map(link => {
-            const { id, href, text } = link;
-            return (
-              <a key={id} href={href} className='capitalize text-lg tracking-wide hover:text-emerald-600 duration-300'>
-                {text}
-              </a>
-            );
-          })}
-        </div>
       </div>
     </nav>
   );
