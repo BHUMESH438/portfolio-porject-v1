0dc10ea805179c2fae5ee8d86bfa8140bd65b9d6
Bhumesh <bhumesh1719@gmail.com>
2023-10-10 09:55:55 +0530

    project title added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,18 +1,19 @@
 import ProjectsCard from './ProjectsCard';
 import { UseFetchProjects } from '../fetchProjects';
+import SectionTitle from './SectionTitle';
 
 const Projects = () => {
   const { loading, projects } = UseFetchProjects();
-  console.log(loading, projects);
   if (loading) {
     return (
       <section className='py-20 align-element'>
-        <h2>Loading....</h2>
+        <h2>Loading......</h2>
       </section>
     );
   }
   return (
     <section className='py-20 align-element' id='projects'>
+      <SectionTitle text='Projects' />
       <div className='py-16 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-8'>
         {projects.map(project => {
           return <ProjectsCard key={project.id} {...project} />;
